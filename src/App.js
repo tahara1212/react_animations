279230479d0f3b2f64b4938ae6da0042423a6ac7
ShunpeiTahara <s-tahara@seraku.co.jp>
2021-09-02 18:04:30 +0900

    create SVG animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { FadeIn } from "./animations/framer_motion/FadeIn";
 import { Transition } from "./animations/framer_motion/Transition";
+import { Circle } from "./animations/svg_animation/Circle";
+import { Rect } from "./animations/svg_animation/Rect";
 
 function App() {
-  return <Transition />;
+  return <Circle />;
 }
 
 export default App;
---
+++ src/animations/svg_animation/Rect.js
@@ -0,0 +1,28 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css, keyframes } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const Rect = () => {
+       return (
+              <SVG width="720" height="720">
+                     <SCircle x="10" y="10" width="380" height="380" rx="10" ry="10" />
+                     <p>testetstet</p>
+              </SVG>
+       )
+}
+
+const SVG = styled.svg`
+       /* transform: rotate(-90deg); */
+`
+const KC = keyframes`
+  0% { stroke-dasharray: 0 2261; }
+  99.9%,to { stroke-dasharray: 2261 2261; }
+`
+const SCircle = styled.rect`
+    fill: transparent;
+    stroke: #df4f4f;
+    stroke-width: 1;
+    animation: ${KC} 3s infinite;
+`
+
